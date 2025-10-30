a59788ad1ff31e5704742944d7a53917d907b15f
manishsorte <mksorte1798@gmail.com>
2025-10-30 22:18:05 +0530

    Initial commit

---
+++ src/test/java/com/system/design/system/design/impl/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.design.system.design.impl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
